ce1575d855c08a4094bcfa630e705094cc256fac
Arnaldo Cicero Bezerra <arnaldotecadm@hotmail.com>
2022-12-18 20:19:44 +0000

    Ajustar estilo do botao do notificationService

--- src/app/shared/notification/notification.service.ts
+++ src/app/shared/notification/notification.service.ts
@@ -42,7 +42,7 @@ export class NotificationService {
         },
         template:
           '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
-          '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+          '<button mat-button  type="button" aria-hidden="true" class="btn close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
           '<i class="material-icons" data-notify="icon">notifications</i> ' +
           '<span data-notify="title" class="notification-title">{1}</span> ' +
           '<span data-notify="message">{2}</span>' +
